ebd64ba4485b8da229c224a4120e6b7f76c2b5c6
vilca942 <vilca942@hotmail.com>
2021-08-19 18:47:49 +0200

    version for heroku deploy

--- config.py
+++ config.py
@@ -15,11 +15,20 @@ SECRET_KEY = "#d#JCqTTW\nilK\\7m\x0bp#\tj~#H"
 
 FB_APP_ID = 1200420960103822  # à supprimer si inutile
 
-"""
-Cette ligne indique à l'ORM SQLAlchemy l'url de la bdd app.db : 
-"""
-basedir = os.path.abspath(os.path.dirname(__file__))
-SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'app.db')
+
+if os.environ.get('DATABASE_URL') is None:
+    """
+    Cette ligne indique à l'ORM SQLAlchemy l'url de la bdd app.db en
+    environnement de dév: 
+    """
+    basedir = os.path.abspath(os.path.dirname(__file__))
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'app.db')
+else:
+    """
+    Cette ligne s'applique si DATABASE_URL a bien été défini par Heroku
+    en environnement de production.
+    """
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
 
 
 SQLALCHEMY_TRACK_MODIFICATIONS = False  # ligne pour éviter un warning de SQLAlchemy
